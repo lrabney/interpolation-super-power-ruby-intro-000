0b654eda01e7e531c606e8a44f54f3d7c85a469c
Lynette Abney <shglynette321@yahoo.com>
2020-02-23 21:25:01 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,4 @@
 def display_rainbow (colors)
-
-
-puts "#{[0], [1], [2], [3], [4], [5], [6]}"
+  colors = ["R:red", "O:orange", "Y:yellow", "G:green", "B:blue", "I:indigo", "V:violet"]
+puts "# {[0],[1],[2],[3],[4],[5],[6]}"
 end
